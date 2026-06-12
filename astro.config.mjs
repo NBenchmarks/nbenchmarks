feb088ff02bee44360b16afd0c7f3bd8aa2a1dbc
NBenchmarks <nbenchmarks@gmail.com>
2026-06-12 16:28:30 -0300

    Updated website

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
+import indexnow from "astro-indexnow";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://nbenchmarks.pages.dev',
   integrations: [
     sitemap({
-      filenameBase: 'sitemap_index' 
+      filenameBase: 'sitemap_index'
+    }),
+    indexnow({
+      key: process.env.INDEXNOW_KEY
     })
   ]
 });
